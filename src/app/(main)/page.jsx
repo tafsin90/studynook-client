d949629101b9e3f12f80108554e04a07712abbae
tafsin90 <tafsin2003@gmail.com>
2026-08-29 21:07:51 +0600

    all done

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -6,14 +6,16 @@ export default async function Home() {
   const sixRooms = await res.json();
 
   return (
-    <div className="space-y-8">
-      <Banner></Banner>
+    <div className=" space-y-8">
+      <Banner />
+
       <h1 className="text-4xl font-semibold text-green-950">Featured Rooms</h1>
+
       <div className="grid grid-cols-1 gap-6 px-6 sm:grid-cols-2 lg:grid-cols-3">
-      {sixRooms.map((room) => (
-        <RoomCard key={room._id} room={room} />
-      ))}
-    </div>
+        {sixRooms.map((room) => (
+          <RoomCard key={room._id} room={room} />
+        ))}
+      </div>
     </div>
   );
 }
